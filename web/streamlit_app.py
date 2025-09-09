10c7893187925766062bcf37212265dbb8190e17
awariict <e.u.awari@gmail.com>
2025-09-09 02:57:05 +0100

    Update streamlit_app.py

--- web/streamlit_app.py
+++ web/streamlit_app.py
@@ -12,7 +12,7 @@ from map_function import show_map   # <--- Import your map function
 # ----------------------------
 # DATABASE CONNECTION
 # ----------------------------
-MONGO_URI = " "mongodb+srv://euawari_db_user:6SnKvQvXXzrGeypA@cluster0.fkkzcvz.mongodb.net/waste_db?retryWrites=true&w=majority";
+MONGO_URI = "mongodb+srv://euawari_db_user:6SnKvQvXXzrGeypA@cluster0.fkkzcvz.mongodb.net/waste_db?retryWrites=true&w=majority";
 
   static final Db _db = Db(_uri);"
 client = MongoClient(MONGO_URI)
